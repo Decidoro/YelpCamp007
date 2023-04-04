b5abc0828774e27ee53d89160fd2018ae0f83d29
Decidoro <gomezdecidoro98@gmail.com>
2023-04-03 21:29:10 -0400

    Basic app setup(requires)

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello from Yelp camp!!.')
+})
+
+app.listen(5500, ()=>{
+    console.log('Serving on port 5500.')
+})
