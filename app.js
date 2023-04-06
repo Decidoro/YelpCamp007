943930e856090455f56a814f5458150942fdc0a0
Decidoro <gomezdecidoro98@gmail.com>
2023-04-05 20:59:02 -0400

    created a list al type page for campgrounds

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const CampGround = require('./models/campground')
+const CampGround = require("./models/campground");
 
 mongoose.set("strictQuery", false);
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
@@ -11,25 +11,28 @@ mongoose.connect("mongodb://localhost:27017/yelp-camp", {
 });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', () =>{
-    console.log('Database connected');
-})
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  console.log("Database connected");
+});
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.get('/', (req, res) =>{
-    res.render('home')
-})
-
-app.get("/makecg", async(req, res) => {
-  const camp = new CampGround({title:'My Backyard', description:'Cheap camping'})
-  await camp.save();
-  res.send(camp);
+app.get("/", (req, res) => {
+  res.render("home");
 });
+
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await CampGround.find({});
+  res.render("./campgrounds/index", { campgrounds }); //structre templates in diffrent folders
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+  res.render('./campgrounds/show')
+})
 
 app.listen(5500, () => {
   console.log("Serving on port 5500.");
